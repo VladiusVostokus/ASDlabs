c2818b1367848cea5b9011c8ce112fee26ae14c4
VladiusVostokus <kovalenkov222@gmail.com>
2023-03-27 17:12:06 +0300

    створення початкового варіанту коду

--- 4(цикл)/main.c
+++ 4(цикл)/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int i,n,x; double Fi;
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    printf("Enter x: ");
+    scanf("%d",&x);
+
+    if (n == 1) Fi = 1;
+
+    else {
+
+        Fi = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            Fi += -Fi * (2*x / 3 - 1);
+
+        }
+
+    }
+
+    printf("Fi = %.5f\n",Fi);
+
     return 0;
 }
+
