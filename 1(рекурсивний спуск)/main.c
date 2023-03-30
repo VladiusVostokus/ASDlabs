ed60ffc777aedeaeea9d19af5083cdeaf6de9b11
VladiusVostokus <kovalenkov222@gmail.com>
2023-03-30 11:12:10 +0300

    робочий варіант алгоритму 1

--- 1(рекурсивний спуск)/main.c
+++ 1(рекурсивний спуск)/main.c
@@ -4,21 +4,22 @@
 
 float recursion(float x, int i,float F_i) {
 
-    if (i < 1)
+    if (i == 1)
         return F_i;
 
     else {
-        F_i = -F_i * (2*x / 3 - 1);
-        recursion(x,i-1,F_i);
+
+        F_i += -F_i * (2*x / 3 - 1);
+        recursion(x, i-1, F_i);
+
     }
 
 }
 
 int main()
 {
-    float res = recursion(1,3,1);
+    float res = recursion(1,5,1);
     printf("%f",res);
 
     return 0;
-
 }
