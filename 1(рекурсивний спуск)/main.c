68519aef79e6b2965dcbdeb10444691e79c85758
VladiusVostokus <kovalenkov222@gmail.com>
2023-03-30 09:51:43 +0300

    перша спроба створити алгоритм 1

--- 1(рекурсивний спуск)/main.c
+++ 1(рекурсивний спуск)/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+float recursion(float x, int i,float F_i) {
+
+    if (i < 1)
+        return F_i;
+
+    else {
+        F_i = -F_i * (2*x / 3 - 1);
+        recursion(x,i-1,F_i);
+    }
+
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    float res = recursion(1,3,1);
+    printf("%f",res);
+
     return 0;
+
 }
