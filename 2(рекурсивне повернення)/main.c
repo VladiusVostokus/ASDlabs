90867bd3da2c84a5965df82722752a339edbe072
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-02 20:03:08 +0300

    перша спроба створити алгоритм 2

--- 2(рекурсивне повернення)/main.c
+++ 2(рекурсивне повернення)/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float recursion2(float x, int n) {
+
+    float F_i = 1;
+
+    if (n == 1)
+
+        recursion2(x, n-1);
+
+    else {
+        F_i += -F_i * (2*x / 3 - 1);
+        return F_i;
+    }
+
+}
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    float res = recursion2(1,5);
+    printf("%f",res);
 }
