0151d64ba143af4762b7f53a2fbc860e23cc7009
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-08 20:36:56 +0300

    рекурсивне повернення(не точний результат)

--- 2(рекурсивне повернення)/main.c
+++ 2(рекурсивне повернення)/main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float recursion2(float x, int n) {
+float recursion2(float x, unsigned int n, float F_i) {
 
-    float F_i = 1;
 
-    if (n == 1)
+    if (n == 1) return F_i;
 
-        recursion2(x, n-1);
 
-    else {
-        F_i += -F_i * (2*x / 3 - 1);
-        return F_i;
-    }
+    else
+        F_i = -F_i * (2*x / 3 - 1) + recursion2(x, n-1, F_i);
+
+
+
+
 
 }
 
 int main()
 {
-    float res = recursion2(1,5);
+    float res = recursion2(1,5,1);
     printf("%f",res);
+
+    return 0;
 }
