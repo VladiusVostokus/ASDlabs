d2ffc36687480fa10b3f43f042ac64a0d257e8d5
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-13 20:19:39 +0300

    виправлення функції

--- 4(цикл)/main.c
+++ 4(цикл)/main.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float recursion_alg()
+float recursion_alg(unsigned int n, float x)
 {
-    int n; float F_i = 1.0; float x;
+    float res = 1; float F_i = 1;
 
-    printf("Enter n: ");
-    scanf("%d",&n);
+    for (int i = 1; i < n; i++) {
 
-    printf("Enter x: ");
-    scanf("%f",&x);
-
-    if (n != 1){
-
-        for (int i = 1; i < n; i++) {
-
-            F_i += -F_i * (2*x / 3 - 1);
-
-        }
+        F_i = -F_i*(2*x / 3 - 1);
+        res += F_i;
+        printf("F_i = %f res = %f\n",F_i,res);
     }
-
     return F_i;
 }
 
+float approximation_func(float x) {
+    return 1.5/x;
+}
+
+
+
 int main()
 {
-    float result = recursion_alg();
-    printf("F_i = %f",result);
+    printf("F_i = %f\n",recursion_alg(5,1.1));
+
+    printf("approximation result = %f\n", approximation_func(1.1));
 
     return 0;
 }
