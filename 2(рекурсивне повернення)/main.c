ab467b8630229563f1a374c8f2101b55d7e9f863
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-14 10:58:03 +0300

    тепер алгоритм 2 рахує члени ряду правильно

--- 2(рекурсивне повернення)/main.c
+++ 2(рекурсивне повернення)/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float recursion2(float x, unsigned int n, float F_i) {
 
+float recursion2(float x, unsigned int n) {
 
-    if (n == 1) return F_i;
+    float F_i;
 
+    if (n == 1)
+        F_i = 1;
 
-    else
-        F_i = -F_i * (2*x / 3 - 1) + recursion2(x, n-1, F_i);
+    else {
+        F_i = -recursion2(x, n-1) * (2*x/3 - 1);
+        printf("F_i = %f\n", F_i);
+    }
 
-
-
-
+    return F_i;
 
 }
 
 int main()
 {
-    float res = recursion2(1,5,1);
+    float res = recursion2(1.1 ,5);
     printf("%f",res);
 
     return 0;
