b371676a8f9f72d0fb3b4bc7a7b46c19d96bd06a
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-21 17:26:54 +0300

    оформлення

--- 4(цикл)/main.c
+++ 4(цикл)/main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float recursion_alg(unsigned int n, float x)
-{
-    float res = 1; float F_i = 1;
-
-    for (int i = 1; i < n; i++) {
-
-        F_i = -F_i*(2*x / 3 - 1);
-        res += F_i;
-        printf("F_i = %f res = %f\n",F_i,res);
-    }
-    return F_i;
-}
-
-float approximation_func(float x) {
-    return 1.5/x;
-}
-
+float cycle_recursion(unsigned int n, float x);
+float approximation_func(float x);
 
 
 int main()
 {
-    printf("F_i = %f\n",recursion_alg(5,1.1));
-
+    printf("Last member = %f\n",cycle_recursion(5,1.1));
     printf("approximation result = %f\n", approximation_func(1.1));
 
     return 0;
 }
 
+float cycle_recursion(unsigned int n, float x)
+{
+    float F_i = 1;
+    float res = F_i;
+
+    for (int i = 1; i < n; i++) {
+
+        F_i = -F_i*(2*x / 3 - 1);
+        res += F_i;
+
+    }
+    printf("Sum = %f\n", res);
+    return F_i;
+}
+
+float approximation_func(float x) {
+
+    return 1.5/x;
+}
