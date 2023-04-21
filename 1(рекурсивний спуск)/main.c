d39dff0eb36a6d0f29da0a0307d31f594b8c4674
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-21 17:25:22 +0300

    оформлення

--- 1(рекурсивний спуск)/main.c
+++ 1(рекурсивний спуск)/main.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float recursion_descent(float x, unsigned int i,float F_i, float sum);
 
-float recursion(float x, unsigned int i,float F_i, float sum) {
+
+int main()
+{
+    float result = recursion_descent(1.1, 5, 1, 0);
+    printf("Last member = %f\n",result);
+
+    return 0;
+}
+
+float recursion_descent(float x, unsigned int i,float F_i, float sum) {
 
     sum += F_i;
-    printf("res = %f ",sum);
 
     if (i == 1){
+
+        printf("Sum = %f\n",sum);
         return F_i;
     }
 
     else {
 
         F_i = -F_i * (2*x / 3 - 1);
-        printf("F_i = %f\n",F_i);
-        recursion(x, i-1, F_i,sum);
+        recursion_descent(x, i-1, F_i,sum);
     }
 
-}
-
-int main()
-{
-    float result = recursion(1.1, 5, 1, 0);
-    printf("%f",result);
-
-    return 0;
 }
