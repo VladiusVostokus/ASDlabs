249b0efd5f29b0bca51548c2eaa263dcb5a9e484
VladiusVostokus <kovalenkov222@gmail.com>
2023-04-21 10:08:10 +0300

    Додав підрахунок суми в програму 2

--- 2(рекурсивне повернення)/main.c
+++ 2(рекурсивне повернення)/main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-float recursion2(float x, unsigned int n) {
+float recursion2(float x, unsigned int n, float *sum) {
 
     float F_i;
 
     if (n == 1)
         F_i = 1;
 
-    else {
-        F_i = -recursion2(x, n-1) * (2*x/3 - 1);
-        printf("F_i = %f\n", F_i);
-    }
+    else
+        F_i = -recursion2(x, n-1, sum) * (2*x/3 - 1);
+
+    *sum += F_i;
 
     return F_i;
-
 }
 
 int main()
 {
-    float res = recursion2(1.1 ,5);
-    printf("%f",res);
+    float sum = 0;
+
+    float res = recursion2(1.1 ,5, &sum);
+
+    printf("Last member = %f\n",res);
+    printf("Sum = %f\n",sum);
 
     return 0;
 }
+
+
+
