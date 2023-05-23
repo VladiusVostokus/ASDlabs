96edff3028ff74a52a424997c73626e521362a6b
VladiusVostokus <kovalenkov222@gmail.com>
2023-05-23 21:21:45 +0300

    cтворення та видалення 2-зв'язного списку n елементів

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ typedef struct linked_list {
 
     int info;
     size_t next_p;
+    size_t prev_p;
 
 } l_list ;
 
@@ -15,10 +16,10 @@ l_list * l_list_init(int item) {
     l_p = malloc(sizeof(struct linked_list));
 
         *l_p = (l_list){ .info = item,
-        .next_p = NULL
+        .next_p = NULL,.prev_p = NULL
     };
 
-    printf("ptr=%d info=%d next=%d\n",l_p,l_p->info,l_p->next_p);
+    printf("ptr=%d info=%d next=%d prev=%d \n",l_p,l_p->info,l_p->next_p,l_p->prev_p);
 
     return l_p;
 }
@@ -28,21 +29,26 @@ l_list * add_list(l_list *l_p, int item) {
 
     struct linked_list *node_p;
     node_p = malloc(sizeof(struct linked_list));
+
+    if (l_p != NULL) l_p->prev_p = node_p;
+
     node_p->info = item;
     node_p->next_p = l_p;
+    node_p->prev_p = NULL;
 
-    printf("ptr=%d info=%d next=%d \n",node_p,node_p->info,node_p->next_p);
+    printf("ptr=%d info=%d next=%d prev=%d \n",node_p,node_p->info,node_p->next_p,node_p->prev_p);
 
     return node_p;
 }
 
-l_list * del_list(l_list *l_p){
+
+l_list * del_list(l_list *l_p) {
 
     size_t node_p = l_p->next_p;
 
     if(!l_p) free(l_p);
 
-    printf("ptr=%d info=%d next=%d \n",l_p,l_p->info,l_p->next_p);
+    printf("ptr=%d info=%d next=%d prev=%d \n",l_p,l_p->info,l_p->next_p,l_p->prev_p);
 
     return node_p;
 }
@@ -53,7 +59,7 @@ int main()
     l_list *list_p;
     int n;
 
-    printf("Enter the count of elemets of list:");
+    printf("Enter the count of elements of list:");
     scanf("%d",&n);
     printf("\n");
 
@@ -71,7 +77,6 @@ int main()
     for(int i = 1; i <= n; i++){
         list_p = del_list(list_p);
     }
-
 
 
     return 0;
